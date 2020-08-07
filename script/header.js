87c9880e8173151ba5ae260cacbf932086f660d5
Stu Ryan <github.com@sturyan.com>
2020-08-06 22:07:14 -0700

    create a header element with javascript

---
+++ script/header.js
@@ -0,0 +1,9 @@
+var headerCnt = 0;
+
+myPageHeader()
+function myPageHeader() {
+  var para = document.createElement("p");
+  para.innerHTML = "<h1>Header</h1>";
+  document.body.appendChild(para);
+}
+
